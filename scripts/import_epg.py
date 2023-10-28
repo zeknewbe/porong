7c2ce9aa6abf9b8cb92a8980a5a4507ae71511da
Chimichurri <132481449+zeknewbe@users.noreply.github.com>
2023-10-28 15:41:28 -0600

    Update import_epg.py

--- scripts/import_epg.py
+++ scripts/import_epg.py
@@ -1,3 +1,23 @@
+import xml.etree.ElementTree as ET
+import requests
+import os
+import base64
+
+GITHUB_REPO_API = 'https://api.github.com/repos/zeknewbe/porong/contents/merged_epg.xml'
+
+def fetch_xml(url):
+    response = requests.get(url)
+    return ET.ElementTree(ET.fromstring(response.text))
+
+def merge_trees(tree1, tree2):
+    root1 = tree1.getroot()
+    root2 = tree2.getroot()
+
+    for child in root2:
+        root1.append(child)
+
+    return tree1
+
 def write_to_github(content, token):
     headers = {
         'Authorization': f'token {token}',
@@ -37,3 +57,17 @@ def write_to_github(content, token):
     except requests.RequestException as e:
         print(f"Error writing to GitHub: {e}")
         exit(1)
+
+if __name__ == "__main__":
+    token = os.environ.get("GITHUB_TOKEN")
+    if not token:
+        print("ERROR: GITHUB_TOKEN not set!")
+        exit(1)
+
+    tree1 = fetch_xml('https://i.mjh.nz/PlutoTV/cl.xml')
+    tree2 = fetch_xml('https://i.mjh.nz/Plex/mx.xml')
+
+    merged_tree = merge_trees(tree1, tree2)
+    merged_xml = ET.tostring(merged_tree.getroot(), encoding='utf-8').decode('utf-8')
+
+    write_to_github(merged_xml, token)
