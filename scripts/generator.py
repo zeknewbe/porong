badbf1e82508e55bb624360551071c0ee00ce06d
Chimichurri <132481449+zeknewbe@users.noreply.github.com>
2023-10-31 14:46:58 -0600

    Update generator.py

--- scripts/generator.py
+++ scripts/generator.py
@@ -8,36 +8,29 @@ import logging
 from logging.handlers import RotatingFileHandler
 import json
 
+# Setup logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  
+logger.setLevel(logging.DEBUG)
 
-log_file = "log.txt" 
+log_file = "log.txt"
 file_handler = RotatingFileHandler(log_file)
-file_handler.setLevel(logging.DEBUG) 
+file_handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
-banner = r'''
-######################################################################
-#  _       _                                          _              #
-# (_)     | |                                        | |             # 
-#  _ _ __ | |___   __  __ _  ___ _ __   ___ _ __ __ _| |_ ___  _ __  #
-# | | '_ \| __\ \ / / / _` |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__| #
-# | | |_) | |_ \ V / | (_| |  __/ | | |  __/ | | (_| | || (_) | |    #
-# |_| .__/ \__| \_/   \__, |\___|_| |_|\___|_|  \__,_|\__\___/|_|    #
-#   | |         ______ __/ |                                         #
-#   |_|        |______|___/                                          #
-#                                                                    #
-#                                                                    #
-######################################################################
+# Constants
+BANNER = '''
+Your Banner Here
 '''
+VALID_URL_SUFFIXES = ('.m3u', '.m3u8', '.ts')
+
 
 def grab(url):
     try:
-        if url.endswith('.m3u') or url.endswith('.m3u8') or ".ts" in url:
+        if url.endswith(VALID_URL_SUFFIXES):
             return url
 
         session = streamlink.Streamlink()
@@ -60,93 +53,104 @@ def check_url(url):
         if response.status_code == 200:
             return True
     except requests.exceptions.RequestException:
-        pass
-    
-    try:
-        response = requests.head(url, timeout=15, verify=False)
-        if response.status_code == 200:
-            return True
-    except requests.exceptions.RequestException:
-        pass
-    
+        try:
+            response = requests.head(url, timeout=15, verify=False)
+            if response.status_code == 200:
+                return True
+        except requests.exceptions.RequestException:
+            pass
     return False
 
-channel_data = []
-channel_data_json = []
 
-channel_info = os.path.abspath(os.path.join(os.path.dirname(__file__), '../channel_info.txt'))
+def process_channel_info(channel_info_path):
+    channel_data = []
 
-with open(channel_info) as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            channel_data.append({
-                'type': 'info',
-                'ch_name': ch_name,
-                'grp_title': grp_title,
-                'tvg_logo': tvg_logo,
-                'tvg_id': tvg_id,
-                'url': ''
-            })
-            
-        else:
-            link = grab(line)
-            if link and check_url(link):
-                channel_data.append({
-                    'type': 'link',
-                    'url': link
-                })
+    try:
+        with open(channel_info_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('~~'):
+                    continue
+                if not line.startswith('https:'):
+                    ch_info = line.split('|')
+                    if len(ch_info) < 4:
+                        logger.error(f"Invalid line format: {line}")
+                        continue
+                    ch_name, grp_title, tvg_logo, tvg_id = [info.strip() for info in ch_info]
+                    channel_data.append({
+                        'type': 'info',
+                        'ch_name': ch_name,
+                        'grp_title': grp_title,
+                        'tvg_logo': tvg_logo,
+                        'tvg_id': tvg_id,
+                        'url': ''
+                    })
+                else:
+                    link = grab(line)
+                    if link and check_url(link):
+                        channel_data.append({
+                            'type': 'link',
+                            'url': link
+                        })
+                    else:
+                        logger.warning(f"Unreachable or unsupported URL: {line}")
 
-with open("playlist.m3u", "w") as f:
-    f.write(banner)
-    f.write(f'\n#EXTM3U')
+    except Exception as e:
+        logger.error(f"Error processing channel_info.txt: {e}")
 
+    return channel_data
+
+
+def main():
+    print(BANNER)
+
+    channel_info_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../channel_info.txt'))
+    channel_data = process_channel_info(channel_info_path)
+
+    # Generate M3U playlist and JSON data
+    playlist_data = ['#EXTM3U']
+    channel_data_json = []
 
     prev_item = None
 
     for item in channel_data:
         if item['type'] == 'info':
             prev_item = item
-        if item['type'] == 'link' and item['url']:
-            f.write(f'\n#EXTINF:-1 group-title="{prev_item["grp_title"]}" tvg-logo="{prev_item["tvg_logo"]}" tvg-id="{prev_item["tvg_id"]}", {prev_item["ch_name"]}')
-            f.write('\n')
-            f.write(item['url'])
-            f.write('\n')
+        elif item['type'] == 'link' and item['url']:
+            playlist_data.extend([
+                f'#EXTINF:-1 group-title="{prev_item["grp_title"]}" tvg-logo="{prev_item["tvg_logo"]}" tvg-id="{prev_item["tvg_id"]}", {prev_item["ch_name"]}',
+                item['url']
+            ])
+            channel_data_json.append({
+                "id": prev_item["tvg_id"],
+                "name": prev_item["ch_name"],
+                "alt_names": [""],
+                "network": "",
+                "owners": [""],
+                "country": "AR",
+                "subdivision": "",
+                "city": "Buenos Aires",
+                "broadcast_area": [""],
+                "languages": ["spa"],
+                "categories": [prev_item["grp_title"]],
+                "is_nsfw": False,
+                "launched": "2016-07-28",
+                "closed": "2020-05-31",
+                "replaced_by": "",
+                "website": item['url'],
+                "logo": prev_item["tvg_logo"]
+            })
+
+    try:
+        with open("playlist.m3u", "w") as f:
+            f.write('\n'.join(playlist_data))
+
+        with open("playlist.json", "w") as f:
+            json.dump(channel_data_json, f, indent=2)
+
+    except Exception as e:
+        logger.error(f"Error writing to file: {e}")
 
 
-prev_item = None
-
-for item in channel_data:
-    if item['type'] == 'info':
-        prev_item = item
-    if item['type'] == 'link' and item['url']:
-        channel_data_json.append( {
-            "id": prev_item["tvg_id"],
-            "name": prev_item["ch_name"],
-            "alt_names": [""],
-            "network": "",
-            "owners": [""],
-            "country": "AR",
-            "subdivision": "",
-            "city": "Buenos Aires",
-            "broadcast_area": [""],
-            "languages": ["spa"],
-            "categories": [prev_item["grp_title"]],
-            "is_nsfw": False,
-            "launched": "2016-07-28",
-            "closed": "2020-05-31",
-            "replaced_by": "",
-            "website": item['url'],
-            "logo": prev_item["tvg_logo"]
-        })
-
-with open("playlist.json", "w") as f:
-    json_data = json.dumps(channel_data_json, indent=2)
-    f.write(json_data)
+if __name__ == "__main__":
+    main()
