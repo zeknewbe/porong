1be6653614277a7d10dfea7bece6c0010fa9dd57
Chimichurri <132481449+zeknewbe@users.noreply.github.com>
2023-11-11 17:40:48 -0600

    Update import_m3u.sh

--- import_m3u.sh
+++ import_m3u.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Variables
-SOURCE_FILE_URL="https://raw.githubusercontent.com/jsosao/m3u/main/mylist.m3u8"
+SOURCE_REPO="jsosao/m3u"  # Replace with the GitHub username/repo
+SOURCE_FILE="mylist.m3u8" # The path to the file in the source repo
+SOURCE_BRANCH="main"      # The branch where the file is located
 DEST_REPO="git@github.com:zeknewbe/porong.git"
-DEST_PATH="mylist.m3u8"     # Path where you want the file to be in the destination repo
+DEST_PATH="mylist.m3u8"   # Path where you want the file to be in the destination repo
 
 # Clone destination repo
 if git clone "$DEST_REPO"; then
@@ -13,18 +15,25 @@ else
     exit 1
 fi
 
-# Download the file using curl
-if ! curl -o "$DEST_PATH" "$SOURCE_FILE_URL"; then
-    echo "Failed to download the file."
+# Add source repo as a remote and fetch the file
+git remote add source_repo "https://github.com/$SOURCE_REPO.git"
+git fetch source_repo $SOURCE_BRANCH
+
+# Checkout the specific file from source repo
+if git checkout source_repo/$SOURCE_BRANCH -- "$SOURCE_FILE"; then
+    mv "$SOURCE_FILE" "$DEST_PATH"  # Move the file to the desired location in the destination repo
+    git add "$DEST_PATH"
+    git commit -m "Imported $SOURCE_FILE from $SOURCE_REPO"
+    if git push origin main; then
+        echo "File imported successfully!"
+    else
+        echo "Failed to push changes to destination repository."
+        exit 1
+    fi
+else
+    echo "Failed to checkout file from source repository."
     exit 1
 fi
 
-# Commit and push the changes
-git add "$DEST_PATH"
-git commit -m "Imported $DEST_PATH from $SOURCE_FILE_URL"
-if git push origin main; then
-    echo "File imported successfully!"
-else
-    echo "Failed to push changes to destination repository."
-    exit 1
-fi
+# Cleanup by removing the added remote
+git remote remove source_repo
