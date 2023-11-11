c0f12d52451029921d4879cd00fbb0c89615f8f1
Chimichurri <132481449+zeknewbe@users.noreply.github.com>
2023-11-11 17:47:42 -0600

    Update import_m3u.sh

--- import_m3u.sh
+++ import_m3u.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
 # Variables
-SOURCE_REPO="jsosao/m3u"  # Replace with the GitHub username/repo
-SOURCE_FILE="mylist.m3u8" # The path to the file in the source repo
-SOURCE_BRANCH="main"      # The branch where the file is located
+SOURCE_REPO="jsosao/m3u"
+SOURCE_FILE="mylist.m3u8"
+SOURCE_BRANCH="main"
 DEST_REPO="git@github.com:zeknewbe/porong.git"
 DEST_PATH="mylist.m3u8"   # Path where you want the file to be in the destination repo
 
+# Setup Git configuration
+GIT_EMAIL="visionintegral@gmail.com"
+GIT_NAME="zeknewbe"
+
 # Clone destination repo
-if git clone "$DEST_REPO"; then
-    cd "$(basename "$DEST_REPO" .git)" || exit
+echo "Cloning destination repository..."
+if git clone "$DEST_REPO" dest-repo; then
+    cd dest-repo || exit 1
 else
     echo "Failed to clone destination repository."
     exit 1
 fi
 
 # Add source repo as a remote and fetch the file
+echo "Adding source repository as remote..."
 git remote add source_repo "https://github.com/$SOURCE_REPO.git"
 git fetch source_repo $SOURCE_BRANCH
 
 # Checkout the specific file from source repo
+echo "Checking out file from source repository..."
 if git checkout source_repo/$SOURCE_BRANCH -- "$SOURCE_FILE"; then
-    mv "$SOURCE_FILE" "$DEST_PATH"  # Move the file to the desired location in the destination repo
-    git add "$DEST_PATH"
-    git commit -m "Imported $SOURCE_FILE from $SOURCE_REPO"
-    if git push origin main; then
-        echo "File imported successfully!"
+    if mv "$SOURCE_FILE" "$DEST_PATH"; then
+        echo "File moved to destination path."
     else
-        echo "Failed to push changes to destination repository."
+        echo "Failed to move file to destination path."
         exit 1
     fi
 else
     echo "Failed to checkout file from source repository."
+    exit 1
+fi
+
+# Commit and push the changes
+echo "Setting up Git configuration..."
+git config user.email "$GIT_EMAIL"
+git config user.name "$GIT_NAME"
+
+echo "Committing changes..."
+git add "$DEST_PATH"
+git commit -m "Imported $SOURCE_FILE from $SOURCE_REPO"
+
+echo "Pushing changes to the repository..."
+if git push origin main; then
+    echo "File imported successfully!"
+else
+    echo "Failed to push changes to destination repository."
     exit 1
 fi
 
