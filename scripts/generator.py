79842dc5a83780130f09a125bb11dbdac81428aa
Chimichurri <132481449+zeknewbe@users.noreply.github.com>
2023-11-12 02:22:32 -0600

    Update generator.py

--- scripts/generator.py
+++ scripts/generator.py
@@ -26,64 +26,51 @@ Your Banner Here
 '''
 VALID_URL_SUFFIXES = ('.m3u', '.m3u8', '.ts')
 
-
 def grab(url):
+    # Directly return the URL if it has a valid suffix
     if url.endswith(VALID_URL_SUFFIXES):
-        logger.debug("URL ends with a valid streaming suffix: %s", url)
-        if check_url(url):
-            return url
-        else:
-            logger.error("Valid streaming URL is not reachable: %s", url)
-            return None
+        logger.debug(f"URL with valid suffix: {url}")
+        return url
 
+    # Use streamlink to grab the actual URL for streams
     try:
         session = streamlink.Streamlink()
         streams = session.streams(url)
-        logger.debug("URL Streams %s: %s", url, streams)
+        logger.debug(f"URL Streams {url}: {streams}")
         if "best" in streams:
             return streams["best"].url
-        return None
-    except streamlink.exceptions.NoPluginError as err:
-        logger.error("URL Error No PluginError %s: %s", url, err)
-        return url
-    except streamlink.StreamlinkError as err:
-        logger.error("URL Error %s: %s", url, err)
-        return None
-
+    except Exception as e:
+        logger.error(f"Streamlink error for URL {url}: {e}")
+    return None
 
 def check_url(url):
+    # Stream the request to avoid downloading content
     try:
-        response = requests.get(url, timeout=15, stream=True)
-        response.raise_for_status()  # Raise an error for bad status codes
-        logger.debug(f"URL is reachable: {url}")
-        return True
+        with requests.get(url, timeout=15, stream=True) as response:
+            response.raise_for_status()  # Raise an error for bad status codes
+            logger.debug(f"URL is reachable: {url}")
+            return True
     except requests.exceptions.RequestException as e:
         logger.error(f"Error checking URL {url}: {e}")
     return False
 
-
 def process_channel_info(channel_info_path):
     channel_data = []
 
-    try:
-        with open(channel_info_path, 'r') as f:
-            for line in f:
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                if not line.startswith('http') and not line.startswith('https'):
-                    logger.error(f"Invalid URL format: {line}")
-                    continue
-                link = grab(line)
-                if link:
-                    channel_data.append({'url': link})
+    with open(channel_info_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            if line.startswith('http') or line.startswith('https'):
+                if check_url(line):
+                    channel_data.append(line)
                 else:
-                    logger.warning(f"Unreachable or unsupported URL: {line}")
-    except Exception as e:
-        logger.error(f"Error processing channel info: {e}")
+                    logger.warning(f"URL check failed: {line}")
+            else:
+                logger.error(f"Invalid URL format: {line}")
 
     return channel_data
-
 
 def main():
     print(BANNER)
@@ -92,18 +79,18 @@ def main():
     channel_data = process_channel_info(channel_info_path)
 
     # Generate M3U playlist
-    playlist_data = ['#EXTM3U']
-    
-    for item in channel_data:
-        if 'url' in item:
-            playlist_data.append(item['url'])
+    playlist_data = ['#EXTM3U'] + channel_data
+
+    # Debug: Print playlist data before writing to the file
+    for line in playlist_data:
+        print(line)
 
     try:
         with open("playlist.m3u", "w") as f:
             f.write('\n'.join(playlist_data))
+        logger.info("Playlist has been written to playlist.m3u")
     except Exception as e:
         logger.error(f"Error writing to playlist.m3u: {e}")
-
 
 if __name__ == "__main__":
     main()
